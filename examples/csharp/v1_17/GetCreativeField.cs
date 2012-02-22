3600d874a1f167cdbf3e5397a348d56d6a6c1e83
api.anash@gmail.com <api.anash@gmail.com@236761dc-7bfb-3f04-0073-daff74cd6533>
2012-02-22 15:35:20 +0000

    Committing v3.1.0 to svn.
    git-svn-id: https://google-api-dfa-dotnet.googlecode.com/svn/branches/v3.1.0@54 236761dc-7bfb-3f04-0073-daff74cd6533

---
+++ examples/csharp/v1_17/GetCreativeField.cs
@@ -0,0 +1,94 @@
+// Copyright 2011, Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Author: api.anash@gmail.com (Anash P. Oommen)
+
+using Google.Api.Ads.Dfa.Lib;
+using Google.Api.Ads.Dfa.v1_17;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Api.Ads.Common.Util;
+
+namespace Google.Api.Ads.Dfa.Examples.CSharp.v1_17 {
+  /// <summary>
+  /// This code example retrieves available creative fields for a given string
+  /// and displays the name, id, advertiser id, and number of values. Results
+  /// are limited to the first 10.
+  ///
+  /// Tags: creativefield.getCreativeFields
+  /// </summary>
+  class GetCreativeField : SampleBase {
+    /// <summary>
+    /// Returns a description about the code example.
+    /// </summary>
+    public override string Description {
+      get {
+        return "This code example retrieves available creative fields for a given string and " +
+            "displays the name, id, advertiser id, and number of values. Results are limited " +
+            "to the first 10.";
+      }
+    }
+
+    /// <summary>
+    /// Main method, to run this code example as a standalone application.
+    /// </summary>
+    /// <param name="args">The command line arguments.</param>
+    public static void Main(string[] args) {
+      SampleBase codeExample = new GetCreativeField();
+      Console.WriteLine(codeExample.Description);
+      codeExample.Run(new DfaUser());
+    }
+
+    /// <summary>
+    /// Run the code example.
+    /// </summary>
+    /// <param name="user">The Dfa user object running the code example.
+    /// </param>
+    public override void Run(DfaUser user) {
+      // Create CreativeFieldRemoteService instance.
+      CreativeFieldRemoteService service = (CreativeFieldRemoteService) user.GetService(
+          DfaService.v1_17.CreativeFieldRemoteService);
+
+      String searchString = _T("INSERT_SEARCH_STRING_CRITERIA_HERE");
+
+      // Set up creative field search criteria structure.
+      CreativeFieldSearchCriteria creativeFieldSearchCriteria = new CreativeFieldSearchCriteria();
+      creativeFieldSearchCriteria.pageSize = 10;
+      creativeFieldSearchCriteria.searchString = searchString;
+
+
+      try {
+        // Get creative fields for the selected criteria.
+        CreativeFieldRecordSet creativeFields =
+            service.getCreativeFields(creativeFieldSearchCriteria);
+
+        // Display creative field names, ids, advertiser ids, and number of values.
+        if (creativeFields != null && creativeFields.records != null) {
+          foreach (CreativeField creativeField in creativeFields.records) {
+            Console.WriteLine("Creative field with name \"{0}\", id \"{1}\", Advertiser id " +
+                "\"{2}\", and containing \"{3}\" values was found.", creativeField.name,
+                creativeField.id, creativeField.advertiserId, creativeField.totalNumberOfValues);
+          }
+        } else {
+          Console.WriteLine("No creative fields found for your search criteria");
+        }
+      } catch (Exception ex) {
+        Console.WriteLine("Failed to retrieve creative fields. Exception says \"{0}\"",
+            ex.Message);
+      }
+    }
+  }
+}
---
+++ examples/csharp/v1_17/RunDeferredReport.cs
@@ -0,0 +1,86 @@
+// Copyright 2011, Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Author: api.anash@gmail.com (Anash P. Oommen)
+
+using Google.Api.Ads.Dfa.Lib;
+using Google.Api.Ads.Dfa.v1_17;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Api.Ads.Common.Util;
+
+namespace Google.Api.Ads.Dfa.Examples.CSharp.v1_17 {
+  /// <summary>
+  /// This code example requests the generation of a report. Once the report
+  /// has been requested, you can continually get updates on the status of the
+  /// report until it is completed. There is currently no way to get a query ID
+  /// through the DFA API; you must use the website interface or the Java DART
+  /// API instead. To request an update on a report's status, run GetReport.cs.
+  ///
+  /// Tags: report.runDeferredReport
+  /// </summary>
+  class RunDeferredReport : SampleBase {
+    /// <summary>
+    /// Returns a description about the code example.
+    /// </summary>
+    public override string Description {
+      get {
+        return "This code example requests the generation of a report. Once the report has been " +
+            "requested, you can continually get updates on the status of the report until it " +
+            "is completed. There is currently no way to get a query ID through the DFA API; " +
+            "you must use the website interface or the Java DART API instead. To request an " +
+            "update on a report's status, run GetReport.cs";
+      }
+    }
+
+    /// <summary>
+    /// Main method, to run this code example as a standalone application.
+    /// </summary>
+    /// <param name="args">The command line arguments.</param>
+    public static void Main(string[] args) {
+      SampleBase codeExample = new RunDeferredReport();
+      Console.WriteLine(codeExample.Description);
+      codeExample.Run(new DfaUser());
+    }
+
+    /// <summary>
+    /// Run the code example.
+    /// </summary>
+    /// <param name="user">The Dfa user object running the code example.
+    /// </param>
+    public override void Run(DfaUser user) {
+      // Create ReportRemoteService instance.
+      ReportRemoteService service = (ReportRemoteService) user.GetService(
+          DfaService.v1_17.ReportRemoteService);
+
+      long queryId = long.Parse(_T("INSERT_QUERY_ID_HERE"));
+
+      // Create report request object.
+      ReportRequest reportRequest = new ReportRequest();
+      reportRequest.queryId = queryId;
+
+      try {
+        // Request generation of a report for your query.
+        ReportInfo reportInfo = service.runDeferredReport(reportRequest);
+
+        // Display success message.
+        Console.WriteLine("Report with ID '{0}' has been scheduled.", reportInfo.reportId);
+      } catch (Exception ex) {
+        Console.WriteLine("Failed to schedule report. Exception says \"{0}\"", ex.Message);
+      }
+    }
+  }
+}
