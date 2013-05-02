df3df1c6a200c5d18ac2d70108d0065940f1a3af
api.anash@gmail.com <api.anash@gmail.com@236761dc-7bfb-3f04-0073-daff74cd6533>
2013-05-02 09:27:06 +0000

    Remove support for v1.17, 1.18.
    git-svn-id: https://google-api-dfa-dotnet.googlecode.com/svn/branches/v3.9.0@79 236761dc-7bfb-3f04-0073-daff74cd6533

---
+++ src/Dfa/Lib/DfaErrorHandler.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2013, Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Author: api.anash@gmail.com (Anash P. Oommen)
+
+using Google.Api.Ads.Common.Lib;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Google.Api.Ads.Dfa.Lib {
+  /// <summary>
+  /// Handles DFA API Errors.
+  /// </summary>
+  public class DfaErrorHandler : ErrorHandler {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DfaErrorHandler"/> class.
+    /// </summary>
+    /// <param name="user">The user.</param>
+    public DfaErrorHandler(AdsUser user)
+      : base(user) {
+    }
+
+    /// <summary>
+    /// Checks if an API call should be retried when an exception occurs.
+    /// </summary>
+    /// <param name="ex">The exception.</param>
+    /// <returns>
+    /// True, if the call should be retried, false otherwise.
+    /// </returns>
+    public override bool ShouldRetry(Exception ex) {
+      return false;
+    }
+
+    /// <summary>
+    /// Prepares the system for retrying the last failed call.
+    /// </summary>
+    /// <param name="ex">The exception.</param>
+    public override void PrepareForRetry(Exception ex) {
+    }
+  }
+}
