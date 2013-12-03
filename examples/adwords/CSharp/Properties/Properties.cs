ea2d09fa57bac9fd2065043d9581d300b49ccc84
unknown <anash@ANASH4-W.ad.corp.google.com>
2013-12-03 10:08:31 -0500

    Pushing v16.4.0 to git.

--- examples/adwords/CSharp/Properties/Properties.cs
+++ examples/adwords/CSharp/Properties/Properties.cs
@@ -20,5 +20,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("AdWords API DotNet Client Library")]
 [assembly: AssemblyCopyright("Copyright 2011, Google Inc. All Rights Reserved.")]
 
-[assembly: AssemblyVersion("16.3.0.0")]
-[assembly: AssemblyFileVersion("16.3.0.0")]
+[assembly: AssemblyVersion("16.4.0.0")]
+[assembly: AssemblyFileVersion("16.4.0.0")]
