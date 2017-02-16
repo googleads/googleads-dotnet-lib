be58e32f50f4c25920e141235853912f2f7acc34
Christopher Seeley <api.cseeley@gmail.com>
2017-02-16 15:35:11 -0500

    Commit v19.0.1 to github

--- examples/AdWords/Vb/Properties/Properties.vb
+++ examples/AdWords/Vb/Properties/Properties.vb
@@ -20,5 +20,5 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyProduct("AdWords API DotNet Client Library")> 
 <Assembly: AssemblyCopyright("Copyright 2011, Google Inc. All Rights Reserved.")> 
 
-<Assembly: AssemblyVersion("19.0.0.0")> 
-<Assembly: AssemblyFileVersion("19.0.0.0")> 
+<Assembly: AssemblyVersion("19.0.1.0")> 
+<Assembly: AssemblyFileVersion("19.0.1.0")> 
