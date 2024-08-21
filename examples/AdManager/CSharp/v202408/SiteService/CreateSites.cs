652b7099b56d509c678eef31ef0694c9728c656d
Chris Seeley <4163216+christopherseeley@users.noreply.github.com>
2024-08-21 13:23:12 +0000

    Commit v24.28.0 to github

---
+++ examples/AdManager/CSharp/v202408/SiteService/CreateSites.cs
@@ -0,0 +1,81 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Ads.AdManager.Lib;
+using Google.Api.Ads.AdManager.v202408;
+
+using System;
+using System.Linq;
+
+namespace Google.Api.Ads.AdManager.Examples.CSharp.v202408
+{
+    /// <summary>
+    /// This code example creates new sites.
+    /// </summary>
+    public class CreateSites : SampleBase
+    {
+        /// <summary>
+        /// Returns a description about the code example.
+        /// </summary>
+        public override string Description
+        {
+            get { return "This code example creates new sites."; }
+        }
+
+        /// <summary>
+        /// Main method, to run this code example as a standalone application.
+        /// </summary>
+        public static void Main()
+        {
+            CreateSites codeExample = new CreateSites();
+            Console.WriteLine(codeExample.Description);
+
+            string childNetworkCode = _T("INSERT_CHILD_NETWORK_CODE_HERE");
+            string url = _T("INSERT_URL_HERE");
+
+            codeExample.Run(new AdManagerUser(), childNetworkCode, url);
+        }
+
+        /// <summary>
+        /// Run the code example.
+        /// </summary>
+        public void Run(AdManagerUser user, string childNetworkCode, string url)
+        {
+            using (SiteService siteService = user.GetService<SiteService>())
+            {
+                Site site = new Site() {
+                    childNetworkCode = childNetworkCode,
+                    url = url
+                };
+
+                try
+                {
+                    // Create the sites on the server.
+                    Site[] sites = siteService.createSites(new Site[] { site });
+
+                    foreach (Site createdSite in sites)
+                    {
+                        Console.WriteLine("A site with ID \"{0}\" and URL \"{1}\" was created.",
+                            createdSite.id, createdSite.url);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Failed to create sites. Exception says \"{0}\"",
+                        e.Message);
+                }
+            }
+        }
+    }
+}
